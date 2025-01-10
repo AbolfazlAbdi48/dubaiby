88c41eed0e6097c952f89827047c7b6e992688d0
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2025-01-10 17:41:16 +0330

    Add: FlightTicket model

--- travel/admin.py
+++ travel/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (
+    FlightTicket,
+)
+
 
 # Register your models here.
+@admin.register(FlightTicket)
+class FlightTicketAdmin(admin.ModelAdmin):
+    pass
--- travel/models.py
+++ travel/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+class FlightTicket(models.Model):
+    TRAVEL_TYPE_CHOICES = [
+        ('ECONOMY', 'اکونومی'),
+        ('PREMIUM_ECONOMY', 'پرمیوم اکونومی'),
+        ('BUSINESS', 'بیزینس'),
+        ('PREMIUM_BUSINESS', 'پرمیوم بیزینس'),
+        ('FIRST', 'فرست'),
+        ('PREMIUM_FIRST', 'پرمیوم فرست')
+    ]
+
+    airline = models.CharField(max_length=100, verbose_name="شرکت هواپیمایی")
+    airline_logo = models.ImageField(upload_to='airline_logos/', null=True, blank=True,
+                                     verbose_name="لوگوی شرکت هواپیمایی")
+    flight_number = models.CharField(max_length=100, verbose_name="شماره پرواز")
+    travel_type = models.CharField(max_length=20, choices=TRAVEL_TYPE_CHOICES, verbose_name="نوع سفر")
+    origin = models.CharField(max_length=100, verbose_name="مبدا")
+    destination = models.CharField(max_length=100, verbose_name="مقصد")
+    departure_date = models.DateTimeField(verbose_name="تاریخ پرواز")
+    arrival_date = models.DateTimeField(verbose_name="تاریخ ورود")
+    price_adult = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="قیمت بزرگسال")
+    price_child = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="قیمت کودک")
+    price_infant = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="قیمت نوزاد")
+
+    def __str__(self):
+        return f"{self.airline} از {self.origin} به {self.destination}"
