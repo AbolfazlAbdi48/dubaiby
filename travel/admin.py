b590de4a9883ad8f3a2f6340d9140d52d271a05e
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2025-01-10 19:05:14 +0330

    Add: Hotel, HotelRoom, Image, Tour model

--- travel/admin.py
+++ travel/admin.py
@@ -1,10 +1,34 @@
 from django.contrib import admin
 from .models import (
     FlightTicket,
+    Hotel,
+    HotelRoom,
+    Image,
+    Tour,
 )
 
 
 # Register your models here.
 @admin.register(FlightTicket)
 class FlightTicketAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(HotelRoom)
+class HotelRoomAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tour)
+class TourAdmin(admin.ModelAdmin):
     pass
--- travel/models.py
+++ travel/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
@@ -12,18 +13,69 @@ class FlightTicket(models.Model):
         ('PREMIUM_FIRST', 'پرمیوم فرست')
     ]
 
-    airline = models.CharField(max_length=100, verbose_name="شرکت هواپیمایی")
+    airline = models.CharField(max_length=100, verbose_name=_("شرکت هواپیمایی"))
     airline_logo = models.ImageField(upload_to='airline_logos/', null=True, blank=True,
-                                     verbose_name="لوگوی شرکت هواپیمایی")
-    flight_number = models.CharField(max_length=100, verbose_name="شماره پرواز")
-    travel_type = models.CharField(max_length=20, choices=TRAVEL_TYPE_CHOICES, verbose_name="نوع سفر")
-    origin = models.CharField(max_length=100, verbose_name="مبدا")
-    destination = models.CharField(max_length=100, verbose_name="مقصد")
-    departure_date = models.DateTimeField(verbose_name="تاریخ پرواز")
-    arrival_date = models.DateTimeField(verbose_name="تاریخ ورود")
-    price_adult = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="قیمت بزرگسال")
-    price_child = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="قیمت کودک")
-    price_infant = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="قیمت نوزاد")
+                                     verbose_name=_("لوگوی شرکت هواپیمایی"))
+    flight_number = models.CharField(max_length=100, verbose_name=_("شماره پرواز"))
+    travel_type = models.CharField(max_length=20, choices=TRAVEL_TYPE_CHOICES, verbose_name=_("نوع سفر"))
+    origin = models.CharField(max_length=100, verbose_name=_("مبدا"))
+    destination = models.CharField(max_length=100, verbose_name=_("مقصد"))
+    departure_date = models.DateTimeField(verbose_name=_("تاریخ پرواز"))
+    arrival_date = models.DateTimeField(verbose_name=_("تاریخ ورود"))
+    price_adult = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("قیمت بزرگسال"))
+    price_child = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("قیمت کودک"))
+    price_infant = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("قیمت نوزاد"))
 
     def __str__(self):
         return f"{self.airline} از {self.origin} به {self.destination}"
+
+
+class Hotel(models.Model):
+    image = models.ImageField(verbose_name=_('تصویر اصلی'), upload_to='hotels/')
+    title = models.CharField(verbose_name=_('عنوان هتل'), max_length=200)
+    location = models.CharField(verbose_name=_('لوکیشن'), max_length=200)
+    rating = models.DecimalField(verbose_name=_('امتیاز'), max_digits=3, decimal_places=2)
+    amenities = models.TextField(verbose_name=_('امکانات'))
+    description = models.TextField(verbose_name=_("توضیحات"))
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    hotel = models.ForeignKey(Hotel, verbose_name=_('هتل'), related_name='images', on_delete=models.CASCADE, null=True,
+                              blank=True)
+    # tour = models.ForeignKey('Tour', verbose_name=_('تور'),
+    # related_name='images', on_delete=models.CASCADE, null=True, blank=True)
+    image = models.ImageField(verbose_name=_('تصویر'), upload_to='images/')
+
+    def __str__(self):
+        return f"{self.hotel.title} - {self.image.url}"
+
+
+class HotelRoom(models.Model):
+    hotel = models.ForeignKey(Hotel, verbose_name=_('هتل'), related_name='rooms', on_delete=models.CASCADE)
+    meal_plan = models.CharField(verbose_name=_('وعده غذایی'), max_length=100)
+    price_per_night = models.DecimalField(verbose_name=_('قیمت هر شب'), max_digits=10, decimal_places=2)
+    description = models.TextField(verbose_name=_('توضیحات'))
+
+    def __str__(self):
+        return f"{self.hotel.title} - {self.meal_plan} - {self.price_per_night}"
+
+
+class Tour(models.Model):
+    image = models.ImageField(verbose_name=_('تصویر'), upload_to='tours/images/')
+    title = models.CharField(verbose_name=_('عنوان'), max_length=200)
+    summary = models.CharField(verbose_name=_('خلاصه'), max_length=200)
+    description = models.TextField(verbose_name=_('توضیحات'))
+    accommodation = models.CharField(verbose_name=_('اقامت'), max_length=200)
+    transport_text = models.CharField(verbose_name=_('حمل و نقل (متن)'), null=True, blank=True, max_length=200)
+    transport_ticket = models.ForeignKey(to='FlightTicket', verbose_name=_('حمل و نقل (بلیط پرواز)'), on_delete=models.SET_NULL,
+                                         null=True, blank=True)
+    meals = models.CharField(verbose_name=_('وعده های غذایی'), max_length=200)
+    start_time = models.DateTimeField(verbose_name=_('زمان شروع'))
+    end_time = models.DateTimeField(verbose_name=_('زمان پایان'))
+    price_per_person = models.DecimalField(verbose_name=_('قیمت به ازای هر نفر'), max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
