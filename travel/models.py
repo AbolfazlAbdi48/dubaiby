0d0709d8aadab3378236badc5b4d3a30b3904d5a
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2025-01-11 12:23:18 +0330

    Update: some models

--- travel/models.py
+++ travel/models.py
@@ -43,24 +43,27 @@ class Hotel(models.Model):
 
 
 class Image(models.Model):
-    hotel = models.ForeignKey(Hotel, verbose_name=_('هتل'), related_name='images', on_delete=models.CASCADE, null=True,
-                              blank=True)
-    # tour = models.ForeignKey('Tour', verbose_name=_('تور'),
-    # related_name='images', on_delete=models.CASCADE, null=True, blank=True)
+    hotel = models.ForeignKey(Hotel, verbose_name=_('هتل'), related_name='hotel_images', on_delete=models.CASCADE,
+                              null=True, blank=True)
+    tour = models.ForeignKey(to='Tour', verbose_name=_('تور'),
+                             related_name='tour_images', on_delete=models.CASCADE, null=True, blank=True)
     image = models.ImageField(verbose_name=_('تصویر'), upload_to='images/')
 
     def __str__(self):
-        return f"{self.hotel.title} - {self.image.url}"
+        if self.hotel:
+            return f"{self.hotel.title} - hotel"
+        return f"{self.tour.title} - tour"
 
 
 class HotelRoom(models.Model):
+    title = models.CharField(verbose_name=_('عنوان'), max_length=200, null=True, blank=True)
     hotel = models.ForeignKey(Hotel, verbose_name=_('هتل'), related_name='rooms', on_delete=models.CASCADE)
     meal_plan = models.CharField(verbose_name=_('وعده غذایی'), max_length=100)
     price_per_night = models.DecimalField(verbose_name=_('قیمت هر شب'), max_digits=10, decimal_places=2)
-    description = models.TextField(verbose_name=_('توضیحات'))
+    description = models.TextField(verbose_name=_('توضیحات'), null=True, blank=True)
 
     def __str__(self):
-        return f"{self.hotel.title} - {self.meal_plan} - {self.price_per_night}"
+        return f"{self.hotel.title} - {self.title}"
 
 
 class Tour(models.Model):
@@ -70,7 +73,8 @@ class Tour(models.Model):
     description = models.TextField(verbose_name=_('توضیحات'))
     accommodation = models.CharField(verbose_name=_('اقامت'), max_length=200)
     transport_text = models.CharField(verbose_name=_('حمل و نقل (متن)'), null=True, blank=True, max_length=200)
-    transport_ticket = models.ForeignKey(to='FlightTicket', verbose_name=_('حمل و نقل (بلیط پرواز)'), on_delete=models.SET_NULL,
+    transport_ticket = models.ForeignKey(to='FlightTicket', verbose_name=_('حمل و نقل (بلیط پرواز)'),
+                                         on_delete=models.SET_NULL,
                                          null=True, blank=True)
     meals = models.CharField(verbose_name=_('وعده های غذایی'), max_length=200)
     start_time = models.DateTimeField(verbose_name=_('زمان شروع'))
